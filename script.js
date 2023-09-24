281b1a748de6a39b69dfaff986e6b26bfcef578e
michaeljbyrne <michaeljbyrne@users.noreply.github.com>
2023-09-23 17:50:59 -0700

    Finish game function, print results

--- script.js
+++ script.js
@@ -19,12 +19,6 @@ function getComputerChoice() {
 return computerChoice;
 }
 
-console.log(getComputerChoice());
-
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-
 
 function playRound(playerSelection,computerSelection) {
 
@@ -33,16 +27,57 @@ function playRound(playerSelection,computerSelection) {
     if (playerChoice === computerSelection) {
         return "Tie game. Play again";
     } else if (playerChoice === "rock" && computerSelection === "paper") {
+        computerScore++;
         return "You lose!  Paper beats rock.";
     } else if (playerChoice === "rock" && computerSelection === "scissors") {
+        playerScore++;
         return "You win!  Rock beats scissors.";
     } else if (playerChoice === "scissors" && computerSelection === "paper") {
+        playerScore++;
         return "You win!  Scissors beats paper.";
     } else if (playerChoice === "scissors" && computerSelection === "rock") {
+        computerScore++;
         return "You lose!  Rock beats scissors.";
     } else if (playerChoice === "paper" && computerSelection === "rock") {
+        playerScore++;
         return "You win!  Paper beats rock.";
     } else if (playerChoice === "paper" && computerSelection === "scissors") {
+        computerScore++;
         return "You lose!  Scissors beats paper.";
     }
 }
+
+
+let computerScore = 0;
+let playerScore = 0;
+
+function game() {
+
+    let keepGoing = true;
+    let playCount = 0;
+
+    while (keepGoing = true) {
+
+        let playerSelection = prompt("Rock, Paper, or Scissors?");
+        let computerSelection = getComputerChoice();
+
+        console.log(playRound(playerSelection,computerSelection));  //play game, return result
+ 
+        playCount++;  // increment playCount for round check in while loop
+
+        console.log(playCount);
+
+        if(computerScore == 3 || playerScore ==3) {    //check whether or not game has been played 5 times
+            keepGoing = false;
+
+            if(computerScore > playerScore) {
+                return `You lose! Computer beat you ${computerScore} - ${playerScore}`;
+            } else {
+            return `You win! You beat computer ${playerScore} - ${computerScore}`;
+            }
+    }
+}
+
+}
+
+console.log(game());
